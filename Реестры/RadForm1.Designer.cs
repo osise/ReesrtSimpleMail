c1580433e367614cd009643a1d7200a5f9214173
S_O <spacecore66@gmail.com>
2021-10-21 09:11:52 +0500

    Добавьте файлы проекта.

---
+++ Реестры/RadForm1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Реестры
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RadForm1));
+            this.fluentTheme1 = new Telerik.WinControls.Themes.FluentTheme();
+            this.radRichTextEditor1 = new Telerik.WinControls.UI.RadTextBoxControl();
+            this.radButton6 = new Telerik.WinControls.UI.RadButton();
+            this.radButton5 = new Telerik.WinControls.UI.RadButton();
+            this.progressBar1 = new Telerik.WinControls.UI.RadProgressBar();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radRichTextEditor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radRichTextEditor1
+            // 
+            this.radRichTextEditor1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.radRichTextEditor1.Location = new System.Drawing.Point(207, 12);
+            this.radRichTextEditor1.Multiline = true;
+            this.radRichTextEditor1.Name = "radRichTextEditor1";
+            this.radRichTextEditor1.Size = new System.Drawing.Size(455, 338);
+            this.radRichTextEditor1.TabIndex = 25;
+            this.radRichTextEditor1.ThemeName = "Fluent";
+            // 
+            // radButton6
+            // 
+            this.radButton6.Font = new System.Drawing.Font("Maiandra GD", 12F);
+            this.radButton6.Location = new System.Drawing.Point(12, 212);
+            this.radButton6.Name = "radButton6";
+            this.radButton6.Size = new System.Drawing.Size(180, 41);
+            this.radButton6.TabIndex = 24;
+            this.radButton6.Text = "Остановить";
+            this.radButton6.ThemeName = "Fluent";
+            this.radButton6.Click += new System.EventHandler(this.radButton6_Click);
+            // 
+            // radButton5
+            // 
+            this.radButton5.Font = new System.Drawing.Font("Maiandra GD", 12F);
+            this.radButton5.Location = new System.Drawing.Point(12, 153);
+            this.radButton5.Name = "radButton5";
+            this.radButton5.Size = new System.Drawing.Size(180, 41);
+            this.radButton5.TabIndex = 23;
+            this.radButton5.Text = "Начать";
+            this.radButton5.ThemeName = "Fluent";
+            this.radButton5.Click += new System.EventHandler(this.radButton5_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(207, 356);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(455, 41);
+            this.progressBar1.TabIndex = 22;
+            this.progressBar1.ThemeName = "Fluent";
+            // 
+            // radButton4
+            // 
+            this.radButton4.Font = new System.Drawing.Font("Maiandra GD", 12F);
+            this.radButton4.Location = new System.Drawing.Point(12, 356);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(180, 41);
+            this.radButton4.TabIndex = 21;
+            this.radButton4.Text = "Выход";
+            this.radButton4.ThemeName = "Fluent";
+            this.radButton4.Click += new System.EventHandler(this.radButton4_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Font = new System.Drawing.Font("Maiandra GD", 12F);
+            this.radButton3.Location = new System.Drawing.Point(12, 12);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(180, 41);
+            this.radButton3.TabIndex = 26;
+            this.radButton3.Text = "Открыть папку с файлами";
+            this.radButton3.ThemeName = "Fluent";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // radButton1
+            // 
+            this.radButton1.Font = new System.Drawing.Font("Maiandra GD", 12F);
+            this.radButton1.Location = new System.Drawing.Point(12, 69);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(180, 41);
+            this.radButton1.TabIndex = 27;
+            this.radButton1.Text = "Выбрать файл с реестром";
+            this.radButton1.ThemeName = "Fluent";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 408);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radRichTextEditor1);
+            this.Controls.Add(this.radButton6);
+            this.Controls.Add(this.radButton5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.radButton4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "     Формирование реестров простых писем v0.9";
+            this.ThemeName = "Fluent";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radRichTextEditor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Telerik.WinControls.Themes.FluentTheme fluentTheme1;
+        private Telerik.WinControls.UI.RadTextBoxControl radRichTextEditor1;
+        private Telerik.WinControls.UI.RadButton radButton6;
+        private Telerik.WinControls.UI.RadButton radButton5;
+        private Telerik.WinControls.UI.RadProgressBar progressBar1;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private Telerik.WinControls.UI.RadButton radButton1;
+    }
+}
---
+++ Реестры/RadForm1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Drawing.Spreadsheet;
+using System.IO;
+using OfficeOpenXml;
+
+namespace Реестры
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        private BackgroundWorker bw = new BackgroundWorker();
+        public string ExcelFilePathFOL { get; set; } = string.Empty;
+        public string ExcelFilePathXLS { get; set; } = string.Empty;
+        string[] ourfiles;
+        string sourcefile = "";
+        DataTable addr = new DataTable();
+        DataColumn column;
+        DataRow row;
+        string fio = "";
+        string address = "";
+        int cou = 0;
+        StringBuilder sb = new StringBuilder();
+        public RadForm1()
+        {
+            InitializeComponent();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        }
+
+        public void OpenDocX(string filename, int rows)
+        {
+            try
+            {
+                string datadocx = "";
+                fio = "";
+                address = "";
+                // Open a WordprocessingDocument for editing using the filepath.
+                WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(filename, false);
+                // Assign a reference to the existing document body.
+                Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+                try
+                {
+                    datadocx = body.ChildElements[4].InnerText; //получаем значение из надписи в документе
+                }
+                catch (Exception ex)
+                {
+                    radRichTextEditor1.Text += "Не могу найти данные в документе. Убедитесь, что содержание документа не изменилось" + "\n";
+                    sb.Append(DateTime.Now + ": Не могу найти данные в документе. Убедитесь, что содержание документа не изменилось\r\n");
+                }
+               
+
+                RegexOptions options = RegexOptions.None;
+                Regex regex = new Regex("[ ]{2,}", options); //Убираем все множественные пробелы и прочие символы
+                datadocx = regex.Replace(datadocx, " ");
+                // foreach (Match m in Regex.Matches(aaa, @"[А-ЯЁ][а-яё]+\s(([А-ЯЁ][а-яё]+\s[А-ЯЁ][а-яё]+)|([А-ЯЁ]\.\s[А-ЯЁ]\.))"))
+                foreach (Match m in Regex.Matches(datadocx, @"[А-ЯЁ][а-яё]+\s(([А-ЯЁ][а-яё]+\s[А-ЯЁ][а-яё]+)|([А-ЯЁ]\.[А-ЯЁ]\.))")) //Отбираем из строки ФИО по формату: "Фамилия И.О.
+                {
+                    fio = m.Value;
+                }
+                address = datadocx.Substring(fio.Length).TrimStart(' '); //Убрать пробел спереди
+
+                row = addr.NewRow();
+                row["fio"] = fio;
+                row["addr"] = address;
+                addr.Rows.Add(row);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+        
+        public void AddDataToReestr(string reestrname)
+        {
+            try
+            {
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                //the path of the file
+                string filePath = reestrname;
+
+                //create a fileinfo object of an excel file on the disk
+                FileInfo file = new FileInfo(filePath);
+
+                //create a new Excel package from the file
+                using (ExcelPackage excelPackage = new ExcelPackage(file))
+                {
+                    //create an instance of the the first sheet in the loaded file
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.First();
+
+                    for (int i = 0; i < addr.Rows.Count; i++)
+                    {
+                        worksheet.Cells[i+2, 1].Value = addr.Rows[i].ItemArray[0];
+                        worksheet.Cells[i+2, 2].Value = addr.Rows[i].ItemArray[1];
+                    }
+                    //add some data
+                    
+
+                    //save the changes
+                    excelPackage.Save();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ExcelFilePathXLS = "";
+                radRichTextEditor1.Text += "\n";
+                sourcefile = "";
+                sb.Append("\r\n");
+                sb.Append("\r\n");
+                sb.Append("------------------------ " + DateTime.Now + " ------------------------\r\n");
+
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Excel Files|*.xlsx";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    sourcefile = Path.GetFileNameWithoutExtension(ofd.SafeFileName);
+                    ExcelFilePathXLS = ofd.FileName;
+                    radRichTextEditor1.Text += "Выбран файл: " + ofd.FileName + "\n";
+                    sb.Append(DateTime.Now + ": Выбран файл: " + ofd.FileName + "\r\n");
+                    string Ext1 = Path.GetExtension(ExcelFilePathXLS);
+                    if (Ext1 == ".xlsx")
+                    {
+                        radRichTextEditor1.Text += "Файл успешно открыт\n";
+                        sb.Append(DateTime.Now + ": Файл успешно открыт\r\n");
+                        if (ExcelFilePathFOL != "" && ExcelFilePathXLS != "")
+                        {
+                            if (ourfiles.Length > 0)
+                            {
+                                radButton5.Enabled = true;
+                                radRichTextEditor1.Text += "Нажмите кнопку Начать\n";
+                            }
+                            else
+                            {
+                                radButton5.Enabled = false;
+                                radRichTextEditor1.Text += "\n";
+                                radRichTextEditor1.Text += "Выберите непустую папку с файлами\n";
+                            }  
+                        }
+                        else
+                        {
+                            radButton5.Enabled = false;
+                            radRichTextEditor1.Text += "\n";
+                            radRichTextEditor1.Text += "Не выбрана папка с файлами и не выбран файл реестра\n";
+                        }
+                    }
+                    else if (Ext1 == ".xls")
+                    {
+                        radRichTextEditor1.Text += "Не удалось открыть файл. Пересохраните файл в формате .xlsx!" + "\n";
+                        sb.Append(DateTime.Now + ": Не удалось открыть файл. Пересохраните файл в формате .xlsx!\r\n");
+                    }
+                    else
+                    {
+                        radRichTextEditor1.Text += "Не удалось открыть файл. Это не файл MS Excel!" + "\n";
+                        sb.Append(DateTime.Now + ": Не удалось открыть файл. Это не файл MS Excel!\r\n");
+                    }
+                }
+                File.AppendAllText(@"C:\Sort-MIV\\log.txt", sb.ToString());
+                sb.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }  //Открытие файла с реестром
+
+        private void radButton4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }  //Выход
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                progressBar1.Text = "Отменено!";
+                radRichTextEditor1.Text += "Отменено!\n";
+                sb.Append("\r\n");
+                sb.Append(DateTime.Now + ": Отменено!\r\n");
+            }
+
+            else if (!(e.Error == null))
+            {
+                progressBar1.Text = ("Ошибка: " + e.Error.Message);
+                radRichTextEditor1.Text += "Ошибка: " + e.Error.Message + "\n";
+                sb.Append("\r\n");
+                sb.Append(DateTime.Now + ": Ошибка: " + e.Error.Message + "\r\n");
+            }
+
+            else
+            {
+                progressBar1.Text = "Выполнено!";
+                radRichTextEditor1.Text += "Выполнено!\n";
+                sb.Append("\r\n");
+                sb.Append(DateTime.Now + ": Выполнено!\r\n");
+            }
+            radRichTextEditor1.Text += "Обработано записей в файле: " + addr.Rows.Count + "\n";
+            sb.Append(DateTime.Now + ": Обработано записей в файле: " + addr.Rows.Count + "\r\n");
+
+
+            File.AppendAllText(@"C:\Sort-MIV\log.txt", sb.ToString());
+            sb.Clear();
+        }  //Отмена
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value1 = e.ProgressPercentage;
+            progressBar1.Text = (e.ProgressPercentage.ToString() + "%");
+        }  //проценты
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ExcelFilePathFOL = "";
+                sb.Append("\r\n");
+                sb.Append("\r\n");
+                sb.Append("------------------------ " + DateTime.Now + " ------------------------\r\n");
+
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    radRichTextEditor1.Text += "\n";
+                    ExcelFilePathFOL = fbd.SelectedPath;
+                    radRichTextEditor1.Text += "Выбрана папка: " + ExcelFilePathFOL + "\n";
+                    sb.Append(DateTime.Now + ": Выбрана папка: " + ExcelFilePathFOL + "\r\n");
+
+                    ourfiles = Directory.GetFiles(ExcelFilePathFOL, "*.docx", SearchOption.AllDirectories);
+                    radRichTextEditor1.Text += "Обнаружено файлов docx в папке: " + ourfiles.Count() + "\n";
+                    sb.Append(DateTime.Now + ": Обнаружено файлов docx в папке: " + ourfiles.Count() + "\r\n");
+
+                    if (ourfiles.Length > 0 || ExcelFilePathXLS != "")
+                    {
+                        radButton5.Enabled = true;
+                        radRichTextEditor1.Text += "Нажмите кнопку Начать\n";
+                    }
+                    else
+                    {
+                        radButton5.Enabled = false;
+                        radRichTextEditor1.Text += "\n";
+                        radRichTextEditor1.Text += "Выберите непустую папку с файлами\n";
+                    }
+                }
+                File.AppendAllText(@"C:\Sort-MIV\\log.txt", sb.ToString());
+                sb.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }  //Открытие папки с файлами
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                BackgroundWorker worker = sender as BackgroundWorker;
+                cou = 0;
+
+                column = new DataColumn();
+                column.DataType = System.Type.GetType("System.String");
+                column.ColumnName = "fio";
+                addr.Columns.Add(column);
+
+                column = new DataColumn();
+                column.DataType = Type.GetType("System.String");
+                column.ColumnName = "addr";
+                addr.Columns.Add(column);
+
+
+                for (int y = 0; y < ourfiles.Length; y++)
+                {
+                    if ((worker.CancellationPending == true))
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    else
+                    {
+                        int percentage = (y + 1) * 100 / ourfiles.Length;
+                        OpenDocX(ourfiles[y], y);
+                        worker.ReportProgress(percentage);
+                    }
+                    File.AppendAllText(@"C:\Sort-MIV\log.txt", sb.ToString());
+                    sb.Clear();
+                }
+                AddDataToReestr(ExcelFilePathXLS);
+
+                radRichTextEditor1.Text += "\n";
+                radRichTextEditor1.Text += "Работа завершена\n";
+
+                radButton5.Enabled = false;
+
+                radRichTextEditor1.Text += "\n";
+                radRichTextEditor1.Text += "Для повторной работы снова выберите файлы\n";
+
+                addr.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void radButton5_Click(object sender, EventArgs e)
+        {
+            if (bw.IsBusy != true)
+            {
+                bw.RunWorkerAsync();
+            }
+        }
+
+        private void radButton6_Click(object sender, EventArgs e)
+        {
+            if (bw.WorkerSupportsCancellation == true)
+            {
+                bw.CancelAsync();
+            }
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            radButton5.Enabled = false;
+            radRichTextEditor1.Text += "1) Выберите папку с файлами для заполнения реестра"  + "\n";
+            radRichTextEditor1.Text += "2) Выберите файл реестра в формате .xlsx"  + "\n";
+            radRichTextEditor1.Text += "3) Нажмите кнопку Начать" + "\n";
+        }
+    }
+}
+
